7a2880654c220b3f9c3fa043b222f3042aab0056
dsouzadyn <dsouzadyn@gmail.com>
2016-03-08 22:09:38 +0530

    Translate docs in Hilo.js

--- src/core/Hilo.js
+++ src/core/Hilo.js
@@ -4,9 +4,10 @@
  * Licensed under the MIT License
  */
 
+
 /**
  * @language=en
- * @namespace Hilo的基础核心方法集合。
+ * @namespace Hilo The underlying core set of methods.
  * @static
  * @module hilo/core/Hilo
  */
@@ -24,9 +25,9 @@ var win = window, doc = document, docElem = doc.documentElement,
 return {
     /**
      * @language=en
-     * 获取一个全局唯一的id。如Stage1，Bitmap2等。
-     * @param {String} prefix 生成id的前缀。
-     * @returns {String} 全局唯一id。
+     * Gets a globally unique id. Such as Stage1, Bitmap2 etc.
+     * @param {String} prefix Generated id's prefix.
+     * @returns {String} Globally unique id.
      */
     /**
      * @language=zh
@@ -46,9 +47,9 @@ return {
 
     /**
      * @language=en
-     * 为指定的可视对象生成一个包含路径的字符串表示形式。如Stage1.Container2.Bitmap3。
-     * @param {View} view 指定的可视对象。
-     * @returns {String} 可视对象的字符串表示形式。
+     * Generates a string representation that contains a path to the specified visual object. Such as Stage1.Container2.Bitmap3.
+     * @param {View} view Specified visual object.
+     * @returns {String} String representation of the visual object.
      */
     /**
      * @language=zh
@@ -67,11 +68,11 @@ return {
 
     /**
      * @language=en
-     * 简单的浅复制对象。
-     * @param {Object} target 要复制的目标对象。
-     * @param {Object} source 要复制的源对象。
-     * @param {Boolean} strict 指示是否复制未定义的属性，默认为false，即不复制未定义的属性。
-     * @returns {Object} 复制后的对象。
+     * Simple shallow copy objects.
+     * @param {Object} target Target object to copy to.
+     * @param {Object} source Source object to copy.
+     * @param {Boolean} strict Indicates whether replication is undefined property, default is false, i.e., undefined attributes are not copied.
+     * @returns {Object} Object after copying.
      */
     /**
      * @language=zh
@@ -92,15 +93,15 @@ return {
 
     /**
      * @language=en
-     * 浏览器特性集合。包括：
+     * Browser feature set includes:
      * <ul>
-     * <li><b>jsVendor</b> - 浏览器厂商CSS前缀的js值。比如：webkit。</li>
-     * <li><b>cssVendor</b> - 浏览器厂商CSS前缀的css值。比如：-webkit-。</li>
-     * <li><b>supportTransform</b> - 是否支持CSS Transform变换。</li>
-     * <li><b>supportTransform3D</b> - 是否支持CSS Transform 3D变换。</li>
-     * <li><b>supportStorage</b> - 是否支持本地存储localStorage。</li>
-     * <li><b>supportTouch</b> - 是否支持触碰事件。</li>
-     * <li><b>supportCanvas</b> - 是否支持canvas元素。</li>
+     * <li><b>jsVendor</b> - Browser vendors js value CSS prefix. For example: webkit.</li>
+     * <li><b>cssVendor</b> - Browser vendors css value CSS prefix.</li>
+     * <li><b>supportTransform</b> - Whether to support CSS Transform transformation.</li>
+     * <li><b>supportTransform3D</b> - Whether to support CSS Transform 3D transformation.</li>
+     * <li><b>supportStorage</b> - Whether to support local stores like localStorage.</li>
+     * <li><b>supportTouch</b> - Whether to support the touch event.</li>
+     * <li><b>supportCanvas</b> - Whether to support the canvas element.</li>
      * </ul>
      */
     /**
@@ -172,11 +173,11 @@ return {
 
     /**
      * @language=en
-     * 事件类型枚举对象。包括：
+     * Event enumeration objects include:
      * <ul>
-     * <li><b>POINTER_START</b> - 鼠标或触碰开始事件。对应touchstart或mousedown。</li>
-     * <li><b>POINTER_MOVE</b> - 鼠标或触碰移动事件。对应touchmove或mousemove。</li>
-     * <li><b>POINTER_END</b> - 鼠标或触碰结束事件。对应touchend或mouseup。</li>
+     * <li><b>POINTER_START</b> - Mouse or touch start event. Corresponds to touchstart or mousedown.</li>
+     * <li><b>POINTER_MOVE</b> - Mouse or touch move event. Corresponds to touchmove or mousemove.</li>
+     * <li><b>POINTER_END</b> - Mouse or touch end event. Corresponds to touchend or mouseup.</li>
      * </ul>
      */
     /**
@@ -199,17 +200,17 @@ return {
 
     /**
      * @language=en
-     * 可视对象对齐方式枚举对象。包括：
+     * Visual object alinment enumeration objects include:
      * <ul>
-     * <li><b>TOP_LEFT</b> - 左上角对齐。</li>
-     * <li><b>TOP</b> - 顶部居中对齐。</li>
-     * <li><b>TOP_RIGHT</b> - 右上角对齐。</li>
-     * <li><b>LEFT</b> - 左边居中对齐。</li>
-     * <li><b>CENTER</b> - 居中对齐。</li>
-     * <li><b>RIGHT</b> - 右边居中对齐。</li>
-     * <li><b>BOTTOM_LEFT</b> - 左下角对齐。</li>
-     * <li><b>BOTTOM</b> - 底部居中对齐。</li>
-     * <li><b>BOTTOM_RIGHT</b> - 右下角对齐。</li>
+     * <li><b>TOP_LEFT</b> - Align the top left corner.</li>
+     * <li><b>TOP</b> - Top center alignment.</li>
+     * <li><b>TOP_RIGHT</b> - Align the top right corner.</li>
+     * <li><b>LEFT</b> - Left center alignment.</li>
+     * <li><b>CENTER</b> - Align center.</li>
+     * <li><b>RIGHT</b> - Right center alignment.</li>
+     * <li><b>BOTTOM_LEFT</b> - Align the bottom left corner.</li>
+     * <li><b>BOTTOM</b> - Bottom center alignment.</li>
+     * <li><b>BOTTOM_RIGHT</b> - Align the bottom right corner.</li>
      * </ul>
      */
     /**
@@ -241,9 +242,9 @@ return {
 
     /**
      * @language=en
-     * 获取DOM元素在页面中的内容显示区域。
-     * @param {HTMLElement} elem DOM元素。
-     * @returns {Object} DOM元素的可视区域。格式为：{left:0, top:0, width:100, height:100}。
+     * Get DOM element content in the page display area.
+     * @param {HTMLElement} elem DOM elements.
+     * @returns {Object} Viewable area DOM elements. Format is: {left:0, top:0, width:100, height:100}.
      */
     /**
      * @language=zh
@@ -256,7 +257,7 @@ return {
             //this fails if it's a disconnected DOM node
             var bounds = elem.getBoundingClientRect();
         }catch(e){
-            bounds = {top:elem.offsetTop, left:elem.offsetLeft, right:elem.offsetLeft + elem.offsetWidth, bottom:elem.offsetTop + elem.offsetHeight};
+            bounds = {top:elem.offsetTop, left:elem.offsetLeft, width:elem.offsetWidth, height:elem.offsetHeight};
         }
 
         var offsetX = ((win.pageXOffset || docElem.scrollLeft) - (docElem.clientLeft || 0)) || 0;
@@ -268,26 +269,23 @@ return {
         var padTop = (parseIntFn(styles.paddingTop) + parseIntFn(styles.borderTopWidth)) || 0;
         var padRight = (parseIntFn(styles.paddingRight) + parseIntFn(styles.borderRightWidth)) || 0;
         var padBottom = (parseIntFn(styles.paddingBottom) + parseIntFn(styles.borderBottomWidth)) || 0;
-
         var top = bounds.top || 0;
         var left = bounds.left || 0;
-        var right = bounds.right || 0;
-        var bottom = bounds.bottom || 0;
 
         return {
             left: left + offsetX + padLeft,
             top: top + offsetY + padTop,
-            width: right - padRight - left - padLeft,
-            height: bottom - padBottom - top - padTop
+            width: bounds.right - padRight - left - padLeft,
+            height: bounds.bottom - padBottom - top - padTop
         };
     },
 
     /**
      * @language=en
-     * 创建一个DOM元素。可指定属性和样式。
-     * @param {String} type 要创建的DOM元素的类型。比如：'div'。
-     * @param {Object} properties 指定DOM元素的属性和样式。
-     * @returns {HTMLElement} 一个DOM元素。
+     * Create a DOM element. You can specify properties and styles.
+     * @param {String} type DOM element type to be created. Such as: 'div'.
+     * @param {Object} properties Properties and styles for DOM element.
+     * @returns {HTMLElement} A DOM element.
      */
     /**
      * @language=zh
@@ -311,9 +309,9 @@ return {
 
     /**
      * @language=en
-     * 根据参数id获取一个DOM元素。此方法等价于document.getElementById(id)。
-     * @param {String} id 要获取的DOM元素的id。
-     * @returns {HTMLElement} 一个DOM元素。
+     * Gets a DOM element according to the parameter id. This method is equivalent to document.getElementById(id).
+     * @param {String} id id of the DOM element you want to get.
+     * @returns {HTMLElement} A DOM element.
      */
     /**
      * @language=zh
@@ -327,8 +325,8 @@ return {
 
     /**
      * @language=en
-     * 设置可视对象DOM元素的CSS样式。
-     * @param {View} obj 指定要设置CSS样式的可视对象。
+     * Set visual object DOM element CSS style.
+     * @param {View} obj Specifies the CSS style to set the visual object.
      * @private
      */
     /**
@@ -416,11 +414,6 @@ return {
     },
 
     /**
-     * @language=en
-     * @private
-     */
-    /**
-     * @language=zh
      * @private
      */
     cacheStateIfChanged: function(obj, propNames, stateCache){
@@ -438,9 +431,9 @@ return {
 
     /**
      * @language=en
-     * 生成可视对象的CSS变换样式。
-     * @param {View} obj 指定生成CSS变换样式的可视对象。
-     * @returns {String} 生成的CSS样式字符串。
+     * Generated visual object CSS style transformation.
+     * @param {View} obj Specifies visual object whose CSS style must be got.
+     * @returns {String} String representation of the CSS style.
      */
     /**
      * @language=zh
