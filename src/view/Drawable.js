dc988a2c883ee081ae60c8dbb3fcd24428a56320
Jonham <jonham@qq.com>
2016-05-09 13:58:05 +0800

    docs: translate src/view/Drawable.js comments to English

--- src/view/Drawable.js
+++ src/view/Drawable.js
@@ -6,13 +6,13 @@
 
 /**
  * @language=en
- * @class Drawable是可绘制图像的包装。
- * @param {Object} properties 创建对象的属性参数。可包含此类所有可写属性。
+ * @class Drawable is a wrapper of drawable images.
+ * @param {Object} properties create Objects properties, contains all writable properties.
  * @module hilo/view/Drawable
  * @requires hilo/core/Hilo
  * @requires hilo/core/Class
- * @property {Object} image 要绘制的图像。即可被CanvasRenderingContext2D.drawImage使用的对象类型，可以是HTMLImageElement、HTMLCanvasElement、HTMLVideoElement等对象。
- * @property {array} rect 要绘制的图像的矩形区域。
+ * @property {Object} image Image to be drawed, can used by CanvasRenderingContext2D.drawImage，like HTMLImageElement、HTMLCanvasElement、HTMLVideoElement。
+ * @property {array} rect The retangle area that image will be drawed.
  */
 /**
  * @language=zh
@@ -34,8 +34,8 @@ var Drawable = Class.create(/** @lends Drawable.prototype */{
 
     /**
      * @language=en
-     * 初始化可绘制对象。
-     * @param {Object} properties 要初始化的属性。
+     * Initialize drawable elements.
+     * @param {Object} properties Properties need to be initialized.
      */
     /**
      * @language=zh
@@ -73,9 +73,9 @@ var Drawable = Class.create(/** @lends Drawable.prototype */{
     Statics: /** @lends Drawable */{
         /**
          * @language=en
-         * 判断参数elem指定的元素是否可包装成Drawable对象。
-         * @param {Object} elem 要测试的对象。
-         * @return {Boolean} 如果是可包装成Drawable对象则返回true，否则为false。
+         * Check whether the given 'elem' and be wrapped into Drawable object.
+         * @param {Object} elem Element to be tested.
+         * @return {Boolean} Return true if element can be wrapped into Drawable element, otherwises return false.
          */
         /**
          * @language=zh
