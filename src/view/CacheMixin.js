368e29d606026f23b6c5561b3b29df6363c61d96
06wj <06wj@163.com>
2017-01-17 15:33:16 +0800

    refactor: cacheMixin create canvas when use

--- src/view/CacheMixin.js
+++ src/view/CacheMixin.js
@@ -4,8 +4,7 @@
  * Licensed under the MIT License
  */
 
-var _cacheCanvas = Hilo.createElement('canvas');
-var _cacheContext = _cacheCanvas.getContext('2d');
+var _cacheCanvas, _cacheContext;
 /**
  * @language=en
  * @class CacheMixin A mixin that contains cache method.You can mix cache method to the target by use Class.mix(target, CacheMixin).
@@ -50,15 +49,22 @@ var CacheMixin = /** @lends CacheMixin# */ {
      * 更新缓存
      */
     updateCache:function(){
-        //TODO:width, height自动判断
-        _cacheCanvas.width = this.width;
-        _cacheCanvas.height = this.height;
-        this._draw(_cacheContext);
-        this._cacheImage = new Image();
-        this._cacheImage.src = _cacheCanvas.toDataURL();
-        this.drawable = this.drawable||new Drawable();
-        this.drawable.init(this._cacheImage);
-        this._cacheDirty = false;
+        if(Hilo.browser.supportCanvas){
+            if(!_cacheCanvas){
+                _cacheCanvas = document.createElement('canvas');
+                _cacheContext = _cacheCanvas.getContext('2d');
+            }
+
+            //TODO:width, height自动判断
+            _cacheCanvas.width = this.width;
+            _cacheCanvas.height = this.height;
+            this._draw(_cacheContext);
+            this._cacheImage = new Image();
+            this._cacheImage.src = _cacheCanvas.toDataURL();
+            this.drawable = this.drawable||new Drawable();
+            this.drawable.init(this._cacheImage);
+            this._cacheDirty = false;
+        }
     },
     /**
      * @language=en
