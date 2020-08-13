378d0e34255d38566a1cd7a44bff955535dd5a1a
06wj <06wj@163.com>
2020-08-13 15:24:58 +0800

    style: fix Camera3d typo

--- src/game/Camera3d.js
+++ src/game/Camera3d.js
@@ -40,148 +40,148 @@
  */
 var Camera3d = (function(){
 
-	var degtorad = Math.PI / 180;
+    var degtorad = Math.PI / 180;
 
-	//Rotate the axis.
-	function rotateX(x, y, z, ca, sa) {//rotate x
-		return {
-			x: x,
-			y: y * ca - z * sa,
-			z: y * sa + z * ca
-		};
-	}
-	function rotateY(x, y, z, ca, sa) {//rotate y
-		return {
-			x: x * ca - z * sa,
-			y: y,
-			z: x * sa + z * ca
-		};
-	}
-	function rotateZ(x, y, z, ca, sa) {//rotate z
-		return {
-			x: x * ca - y * sa,
-			y: x * sa + y * ca,
-			z: z
-		};
-	}
+    //Rotate the axis.
+    function rotateX(x, y, z, ca, sa) {//rotate x
+        return {
+            x: x,
+            y: y * ca - z * sa,
+            z: y * sa + z * ca
+        };
+    }
+    function rotateY(x, y, z, ca, sa) {//rotate y
+        return {
+            x: x * ca - z * sa,
+            y: y,
+            z: x * sa + z * ca
+        };
+    }
+    function rotateZ(x, y, z, ca, sa) {//rotate z
+        return {
+            x: x * ca - y * sa,
+            y: x * sa + y * ca,
+            z: z
+        };
+    }
 
-	var Camera3d = Class.create(/** @lends Camera3d.prototype */{
+    var Camera3d = Class.create(/** @lends Camera3d.prototype */{
 
-		constructor: function(properties){
-			properties.x = properties.x || 0;
-			properties.y = properties.y || 0;
-			properties.z = properties.z || 0;
-			properties.rotationX = properties.rotationX || 0;
-			properties.rotationY = properties.rotationY || 0;
-			properties.rotationZ = properties.rotationZ || 0;
+        constructor: function(properties){
+            properties.x = properties.x || 0;
+            properties.y = properties.y || 0;
+            properties.z = properties.z || 0;
+            properties.rotationX = properties.rotationX || 0;
+            properties.rotationY = properties.rotationY || 0;
+            properties.rotationZ = properties.rotationZ || 0;
 
-        	util.copy(this, properties);
-		},
+            util.copy(this, properties);
+        },
 
-	    /**
+        /**
          * @language=en
          * Translate the camera，used for Zoomin/out feature.
-	     * @param {Number} x The x position.
-	     * @param {Number} y The y position.
-	     * @param {Number} z The z position.
-	     */
+         * @param {Number} x The x position.
+         * @param {Number} y The y position.
+         * @param {Number} z The z position.
+         */
         /**
          * @language=zh
          * 仿射矩阵位移变换，不同于直接修改Camera3d.x/y/z. 是在Camera3d依次做坐标位移 - 旋转变换 后，再加上一个位移变换。主要功能可以做Zoomin/out 功能
-	     * @param {Number} x x坐标
-	     * @param {Number} y y坐标
-	     * @param {Number} z z坐标
-	     */
-		translate : function(x,y,z){
-			this.tx = x;
-			this.ty = y;
-			this.tz = z;
-		},
+         * @param {Number} x x坐标
+         * @param {Number} y y坐标
+         * @param {Number} z z坐标
+         */
+        translate : function(x,y,z){
+            this.tx = x;
+            this.ty = y;
+            this.tz = z;
+        },
 
-	    /**
+        /**
          * @language=en
          * Rotate by the x axis.
-	     * @param {Number} angle The rotate degree.
-	     */
+         * @param {Number} angle The rotate degree.
+         */
         /**
          * @language=zh
          * 旋转X轴方向角度，相当于欧拉角系统的 beta。
-	     * @param {Number} angle 旋转角度。
-	     */
-		rotateX : function(angle){
-			this.rotationX = angle;
-		},
+         * @param {Number} angle 旋转角度。
+         */
+        rotateX : function(angle){
+            this.rotationX = angle;
+        },
 
-	    /**
+        /**
          * @language=en
          * Rotate by the y axis.
-	     * @param {Number} angle The rotate degree.
-	     */
+         * @param {Number} angle The rotate degree.
+         */
         /**
          * @language=zh
          * 旋转Y轴方向角度，相当于欧拉角系统的 gamma。
-	     * @param {Number} angle 旋转角度。
-	     */
-		rotateY : function(angle){
-			this.rotationY = angle;
-		},
+         * @param {Number} angle 旋转角度。
+         */
+        rotateY : function(angle){
+            this.rotationY = angle;
+        },
 
-	    /**
+        /**
          * @language=en
          * Rotate by the z axis.
-	     * @param {Number} angle The rotate degree.
-	     */
+         * @param {Number} angle The rotate degree.
+         */
         /**
          * @language=zh
          * 旋转Z轴方向角度，相当于欧拉角系统的 alpha。
-	     * @param {Number} angle 旋转角度。
-	     */
-		rotateZ : function(angle){
-			this.rotationZ = angle;
-		},
+         * @param {Number} angle 旋转角度。
+         */
+        rotateZ : function(angle){
+            this.rotationZ = angle;
+        },
 
-	    /**
+        /**
          * @language=en
          * Project the 3d point to 2d point.
-	     * @param {object} vector3D The 3d position, it must have x, y and z properties.
-	     * @param {View} view The view related to the 3d position.It'll be auto translated by the 3d position.
+         * @param {object} vector3D The 3d position, it must have x, y and z properties.
+         * @param {View} view The view related to the 3d position.It'll be auto translated by the 3d position.
          * @returns {Object} The 2d object include z and scale properties, e.g.:{x:x, y:y, z:z, scale}
-	     */
+         */
         /**
          * @language=zh
          * 将三维坐标转换投影为二维坐标。
-	     * @param {object} vector3D 三维坐标对象，必须含有x, y, z属性。
-	     * @param {View} view Hilo.View对象，用于自动转换坐标。
+         * @param {object} vector3D 三维坐标对象，必须含有x, y, z属性。
+         * @param {View} view Hilo.View对象，用于自动转换坐标。
          * @returns {Object} 二维坐标对象，包括缩放和z属性，例子:{x:x, y:y, z:z, scale}
-	     */
-		project : function(vector3D, view){
+         */
+        project : function(vector3D, view){
 
-			var rx = this.rotationX * degtorad,
-				ry = this.rotationY * degtorad,
-				rz = this.rotationZ * degtorad,
+            var rx = this.rotationX * degtorad,
+                ry = this.rotationY * degtorad,
+                rz = this.rotationZ * degtorad,
 
-				cosX = Math.cos(rx), sinX = Math.sin(rx),
-				cosY = Math.cos(ry), sinY = Math.sin(ry),
-				cosZ = Math.cos(rz), sinZ = Math.sin(rz),
+                cosX = Math.cos(rx), sinX = Math.sin(rx),
+                cosY = Math.cos(ry), sinY = Math.sin(ry),
+                cosZ = Math.cos(rz), sinZ = Math.sin(rz),
 
-				// 旋转变换前的 仿射矩阵位移，
-				dx = vector3D.x - this.x,
-				dy = vector3D.y - this.y,
-				dz = vector3D.z - this.z;
+                // 旋转变换前的 仿射矩阵位移，
+                dx = vector3D.x - this.x,
+                dy = vector3D.y - this.y,
+                dz = vector3D.z - this.z;
 
-			// 旋转矩阵变换
-			var vector = rotateZ(dx, dy, dz, cosZ, sinZ);
-			vector = rotateY(vector.x, vector.y, vector.z, cosY, sinY);
-			vector = rotateX(vector.x, vector.y, vector.z, cosX, sinX);
+            // 旋转矩阵变换
+            var vector = rotateZ(dx, dy, dz, cosZ, sinZ);
+            vector = rotateY(vector.x, vector.y, vector.z, cosY, sinY);
+            vector = rotateX(vector.x, vector.y, vector.z, cosX, sinX);
 
-			// 最后的仿射矩阵变换
-			if(this.tx) vector.x -= this.tx;
-			if(this.ty) vector.y -= this.ty;
-			if(this.tz) vector.z -= this.tz;
+            // 最后的仿射矩阵变换
+            if(this.tx) vector.x -= this.tx;
+            if(this.ty) vector.y -= this.ty;
+            if(this.tz) vector.z -= this.tz;
 
-			var	perspective = this.fv / (this.fv + vector.z),
-				_x = vector.x * perspective,
-				_y = -vector.y * perspective;
+            var perspective = this.fv / (this.fv + vector.z),
+                _x = vector.x * perspective,
+                _y = -vector.y * perspective;
 
             var result = {
                 x : _x + this.fx,
@@ -190,45 +190,45 @@ var Camera3d = (function(){
                 scale : perspective
             };
 
-			if(view){
+            if(view){
                 view.x = result.x;
                 view.y = result.y;
                 view.z = result.z;
                 view.scaleX = result.scale;
                 view.scaleY = result.scale;
-			}
+            }
 
             return result;
-		},
+        },
 
-	    /**
+        /**
          * @language=en
          * Sort by z axis.
-	     */
+         */
         /**
          * @language=zh
          * Z深度排序。
-	     */
-		sortZ : function(){
+         */
+        sortZ : function(){
             this.stage.sortChildren(function(view_a, view_b){
                 return view_a.z - view_b.z;
-            })
-		},
+            });
+        },
 
-	    /**
+        /**
          * @language=en
          * Used for the ticker.
-	     */
+         */
         /**
          * @language=zh
          * Ticker 轮询使用。
-	     */
-		tick : function(){
-			this.sortZ();
-		}
+         */
+        tick : function(){
+            this.sortZ();
+        }
 
-	});
+    });
 
-	return Camera3d;
+    return Camera3d;
 
 })();
